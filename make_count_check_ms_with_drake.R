21431724ca7f86dfb2174103442838fb0f4aaff6
richardjtelford <richard.telford@bio.uib.no>
2019-02-06 19:20:56 +0100

    make it render

--- make_count_check_ms_with_drake.R
+++ make_count_check_ms_with_drake.R
@@ -10,28 +10,29 @@ library("rjt.misc")
 #import scripts
 
 
-
+#drake configuration
+pkgconfig::set_config("drake::strings_in_dots" = "literals")
 
 #construct drake plan
 analyses <- drake_plan(
   
   #import & clean data
+  
 
   #make plots
   
-  #get bibliography
-  biblio = download.file("https://raw.githubusercontent.com/richardjtelford/Zabinskie/master/chironomid.bib", destfile = file_out("Rmd/extra/chironomid.bib")),
-  
   #add extra packages to bibliography
   biblio2 = package_citations(
-    packages = c("vegan", "rioja", "analogue", "palaeoSig"), 
-    old_bib = file_in("Rmd/extra/chironomid.bib"), 
-    new_bib = file_out("Rmd/extra/chironomid2.bib")),
+    packages = c("rioja"), 
+    old_bib = file_in("Rmd/extra/countMS.bib"), 
+    new_bib = file_out("Rmd/extra/countMS2.bib")),
   
   #knit manuscript
-  manuscript = rmarkdown::render(input = knitr_in("Rmd/count_check_MS.Rmd"), output_dir = "./output", output_file = file_out("output/count_check_MS.pdf")),
-  
-  strings_in_dots = "literals"
+  #knit manuscript
+  manuscript = target(
+    command = rmarkdown::render(input = knitr_in("Rmd/count_check_MS.Rmd"), output_dir = "./output", output_file = file_out("output/count_check_MS.pdf")), 
+    trigger = trigger(change =list(biblio2))
+  )
 )
 
 #configure and make drake plan
@@ -39,5 +40,7 @@ config <- drake_config(analyses)
 outdated(config)        # Which targets need to be (re)built?
 make(analyses)          # Build the right things.
 
-#voew dependency graph
+system("evince output/count_check_MS.pdf", wait = FALSE)#display pdf - only linux
+
+#view dependency graph
 vis_drake_graph(config, targets_only = TRUE)
