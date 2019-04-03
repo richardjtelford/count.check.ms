bc64c6bbbd8844a5e61e3c2b8f24c18e0585e7c2
richardjtelford <richard.telford@bio.uib.no>
2019-04-04 00:30:06 +0200

    moved bird functions to new file

---
+++ R/bird_functions.R
@@ -0,0 +1,36 @@
+#resample to smaller count sum with multivariate hypergeometric & count singletons
+recount_singletons <- function(counts, k, nrep = 10) {
+  k %>% set_names() %>%
+    map_df(~{
+      y <- extraDistr::rmvhyper(nn = nrep, n = counts, k = .)
+      tibble(no_singletons = rowSums(y == 1), rep = 1:nrep)
+    }, .id = "new_count_sum")
+}
+
+#functions
+#join and process at different taxonomic levels
+
+bird_process <- function(bird_data, bird_species) {
+  joined <- bird_data %>%
+    left_join(bird_species, by = "AOU")
+  
+  out <- bind_rows(
+    species = joined %>% rename(taxon = Latin_name),
+    genus = joined %>% rename(taxon = Genus),
+    family = joined %>% rename(taxon = Family),
+    order = joined %>% rename(taxon = ORDER),
+    .id = "taxonomic_level"
+  ) %>%
+    group_by(RouteDataID, year, taxonomic_level, taxon) %>%
+    summarise(count = sum(count)) %>%
+    group_by(RouteDataID, year, taxonomic_level) %>%
+    summarise(
+      n_taxa = n(),
+      singletons = sum(count == 1),
+      count_sum = sum(count), 
+      GCD = ifelse(singletons == 0 & n_taxa > 1, numbers::mGCD(count), NA)#only find GCD where no singletons for speed ( and > 1 taxa). Using ifelse as it doesn't run TRUE code when it would crash
+    )
+  return(out)
+}
+
+
--- R/download_birds.R
+++ R/download_birds.R
@@ -1,4 +1,5 @@
 #breeding bird survey
+source("R/bird_functions.R")
 
 bird_plan <- drake_plan(
   #download data
@@ -29,80 +30,46 @@ bird_plan <- drake_plan(
     map_df(select, -starts_with("Stop")),
   
   #aggregate to different taxonomic levels
-  bird_singletons = bird_process(bird_data, bird_species)
+  bird_singletons = bird_process(bird_data, bird_species),
 
-)
-
-bird_singletons %>% 
-  group_by(taxonomic_level) %>%   
-  summarise(mean_singletons = mean(singletons == 0), n_singletons = sum(singletons == 0), mean_richness = mean(n_species))
-
+  bird_singleton_summary = bird_singletons %>% 
+    group_by(taxonomic_level) %>%   
+    summarise(pc_singletons = mean(singletons == 0) * 100, n_singletons = sum(singletons == 0), mean_richness = mean(n_taxa), n_routes = n(), GCD = mean(GCD > 1, na.rm = TRUE) * 100) %>% 
+    mutate(taxonomic_level = factor(taxonomic_level, levels = c("species", "genus", "family", "order"))) %>% 
+    arrange(taxonomic_level),
   
-
-cf <- drake_config(bird_plan)
-make(bird_plan)
-
-loadd(bird_data)
-
-
-p1 <- nsf %>%
-  mutate(no_si = singletons == 0) %>% 
-  ggplot(aes(x = n_species, fill = no_si)) +
-  geom_bar()+
-  facet_wrap(~no_si, ncol = 1, scales = "free_y")
-p1
-p1 + aes(x = count_sum) + scale_x_log10()
-
-bird_recount_singletons = bird_data %>% 
-  group_by(RouteDataID) %>% 
-  filter(sum(count) >= 400) %>% 
-  left_join(bird_species, by = "AOU") %>% 
-  group_by(RouteDataID, ORDER) %>% 
-  summarise(count = sum(count)) %>% 
-  nest() %>% 
-  mutate(recount = map(data, ~recount_singletons(counts = .$count, k = c(10, 20, 30, 50, 100, 200, 300, 400)))) %>% 
-  unnest(recount)
-
-bird_recount_singletons %>% 
-  mutate(new_count_sum = as.numeric(new_count_sum)) %>% 
-  group_by(new_count_sum, rep) %>% 
-  summarise(m = mean(no_singletons == 0)) %>% 
-  summarise(m_no_singletons = mean(m), sd = sd(m), se = sd/sqrt(n()), n = n())
-
-.Last.value %>% ggplot(aes(x = new_count_sum, y = m_no_singletons, ymin = m_no_singletons - 1.96 * se, ymax = m_no_singletons + 1.96 * se)) + geom_pointrange()
-
-#resample to smaller count sum with multivariate hypergeometric & count singletons
-recount_singletons <- function(counts, k, nrep = 10) {
- k %>% set_names() %>%
-   map_df(~{
-     y <- extraDistr::rmvhyper(nn = nrep, n = counts, k = .)
-     tibble(no_singletons = rowSums(y == 1), rep = 1:nrep)
-   }, .id = "new_count_sum")
-}
-
-#functions
-#join and process at different taxonomic levels
-
-bird_process <- function(bird_data, bird_species) {
-  joined <- bird_data %>%
-    left_join(bird_species, by = "AOU")
+  #bird order GCD summary
+  bird_order_GCD = bird_singletons %>% 
+    ungroup() %>% 
+    filter(singletons == 0, taxonomic_level == "order", !is.na(GCD)) %>%
+    group_by(n_taxa) %>% 
+    summarise(t = sum(GCD > 1, na.rm = TRUE), n = n()),
   
-  out <- bind_rows(
-    species = joined %>% rename(taxon = Latin_name),
-    genus = joined %>% rename(taxon = Genus),
-    family = joined %>% rename(taxon = Family),
-    order = joined %>% rename(taxon = ORDER),
-    .id = "taxonomic_level"
-  ) %>%
-    group_by(RouteDataID, year, taxonomic_level, taxon) %>%
-    summarise(count = sum(count)) %>%
-    group_by(RouteDataID, year, taxonomic_level) %>%
-    summarise(
-      n_species = n(),
-      singletons = sum(count == 1),
-      count_sum = sum(count)
-    )
-  return(out)
-}
+  #simulate counts with mv hypergeometric
+  bird_recount_singletons = bird_data %>% 
+    group_by(RouteDataID) %>% 
+    filter(sum(count) >= 400) %>% 
+    left_join(bird_species, by = "AOU") %>% 
+    group_by(RouteDataID, ORDER) %>% 
+    summarise(count = sum(count)) %>% 
+    nest() %>% 
+    mutate(recount = map(data, ~recount_singletons(counts = .$count, k = c(30, 50, 100, 200, 300, 400)))) %>% 
+    unnest(recount) %>% 
+    mutate(new_count_sum = as.numeric(new_count_sum)) %>% 
+    group_by(new_count_sum, rep) %>% 
+    summarise(m = mean(no_singletons == 0)) %>% 
+    summarise(m_no_singletons = mean(m), sd = sd(m), se = sd/sqrt(n())),
+  
+  bird_recount_singleton_plot = bird_recount_singletons %>% 
+    ggplot(aes(x = new_count_sum, y = m_no_singletons, ymin = m_no_singletons - 1.96 * se, ymax = m_no_singletons + 2 * se)) + 
+    geom_errorbar() + 
+    geom_point(size = 0.5) +
+    labs(x = "Count sum", y = "Proportion without singletons")
+  )#end of drake_plan
+ 
+
+# cf <- drake_config(bird_plan)
+# make(bird_plan, keep_going = TRUE)
+# vis_drake_graph(cf)
 
 
