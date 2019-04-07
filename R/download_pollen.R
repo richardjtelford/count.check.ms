032acb34d2b32ae8aa2f9cfc8c52bd14ebcac2d7
richardjtelford <richard.telford@bio.uib.no>
2019-04-07 12:02:29 +0200

    more summary options

--- R/download_pollen.R
+++ R/download_pollen.R
@@ -19,18 +19,25 @@ pollen_plan <- drake_plan(
  #summarise
  pollen1_summary = list(
    count = pollen1_counts %>% 
-     summarise(GCD = numbers::mGCD(count), min = min(count), n = n()) %>% 
-     summarise(has_singletons = sum(min == 1), GCD2 = sum(GCD > 1)),
+     summarise(GCD = numbers::mGCD(count), min = min(count), n = n(), sum = sum(count)) %>% 
+     summarise(has_singletons = sum(min == 1), GCD2 = sum(GCD > 1), GCD4 = sum(GCD ==4), min_count = min(sum), max_count = max(sum), max2_count = max(sum[-which.max(sum)])),
+   
    individual = pollen1_counts %>% 
      ungroup() %>%
-     summarise(one = sum(count == 1), two = sum(count %% 2 == 0), n = n(), not_two = n - two)
+     summarise(one = sum(count == 1), two = sum(count %% 2 == 0), n = n(), not_two = n - two),
+   
+   max_trilete =  pollen1_counts %>% 
+     filter(taxa == "Unknown (trilete)") %>% 
+     ungroup() %>% 
+     summarise(m = max(count)) %>% 
+     pull(m)
  )
 )
 
 
-cf <- drake_config(pollen_plan)
-make(pollen_plan, keep_going = TRUE)
-vis_drake_graph(cf)
+# cf <- drake_config(pollen_plan)
+# make(pollen_plan, keep_going = TRUE)
+# vis_drake_graph(cf)
 
 
 
