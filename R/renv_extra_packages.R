e9dd51ca8dfb091b5f17ca22a5b10f6b6e0933c1
richardjtelford <richard.telford@bio.uib.no>
2020-08-19 23:05:19 +0200

    list of dependencies not identified by renv

---
+++ R/renv_extra_packages.R
@@ -0,0 +1,4 @@
+#extra packages required but recognised by renv (probably suggests rather than imports field of package description file)
+requireNamespace("visNetwork")
+requireNamespace("rticles")
+requireNamespace("curl")
