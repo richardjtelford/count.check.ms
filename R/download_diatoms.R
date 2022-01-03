969c420c93f839996918f16324e2e9a9f2286c9b
richardjtelford <richard.telford@bio.uib.no>
2022-01-03 19:14:49 +0100

    use molten data

--- R/download_diatoms.R
+++ R/download_diatoms.R
@@ -1,69 +1,40 @@
 #diatoms
 diatom_plan <- drake_plan(
-  ####Owens Lake####
-  owens = read_file(file = "http://diatom.ansp.org/dpdc/download.asp?type=count&DatasubsetID=51") %>% 
-    gsub(pattern = "<.*?>", replacement = "", x = .) %>% 
-    read_delim(delim = "\t"),
+  #### MOLTEN ####
   
-  owens_summ1 = owens %>% 
-  group_by(SampleId) %>% 
-  summarise(mn = min(Count), n_sing = sum(Count == 1), count_sum = sum(Count), n_taxa = n(), gcd = ifelse(n_taxa > 1, numbers::mGCD(Count), NA)),
-  
-  owens_summ = owens_summ1 %>% 
-    filter(n_taxa > 1) %>% 
-    summarise(
-      n = n(),
-      count_min = min(count_sum),
-      count_max = max(count_sum),
-      median_sing = median(n_sing),
-      p_singletons  = mean(n_sing  > 0) * 100,
-      no_singletons = mean(mn != 1) * 100,
-      no_single_gt_50 = mean(mn[count_sum > 50] == 1) * 100, 
-      no_single50_ = mean(mn[count_sum <= 50] == 1) * 100, 
-      gcd2 = mean(gcd == 1) * 100,
-      gcd_max = max(gcd, na.rm = TRUE)),
-  
-  ####owens diatoms percent####
-  owens_est = owens %>% 
-    estimate_n(ID_cols = c("SampleId", "SampleCode", "CountSum"), percent_col = "Percent", taxon_col = "TaxonShortName", digits = 4) %>%
-    assertr::verify(map_int(direct_search_est, nrow) == 1) %>% #check only one row in each direct_search_est
-    unnest(direct_search_est) %>% 
-    assertr::verify(score == 1),#check score ==1
-  
-  owens_est_summ = owens_est %>% 
-    select(est_n_minpc, est_n_direct, CountSum, n_taxa) %>% 
-    filter(n_taxa > 1) %>% 
-    ungroup() %>% 
-    summarise(
-      minpc = mean(round(est_n_minpc, 3) == CountSum) * 100, 
-      direct = mean(est_n_direct == CountSum) * 100
-    ),
-  
-  owens_fail = owens_est %>% 
-    select(est_n_minpc, est_n_direct, CountSum, n_taxa) %>%
-    filter(n_taxa > 1, est_n_direct != CountSum),
-  
-  owens_fail_minpc = owens_est %>% 
-    select(est_n_minpc, est_max_minpc, est_min_minpc, CountSum, n_taxa) %>%
-    filter(n_taxa > 1, !between(CountSum, est_min_minpc, est_max_minpc)),
-  
-  owens_plot = owens_est %>% 
-    filter(between(n_taxa, 8, 15), between(CountSum, 80, 150)) %>% 
-    ungroup() %>% 
-    slice(4) %>%  
-    select(c(SampleId:n_taxa), score_best = score, est_n_direct) %>% 
-    unnest(direct_search) %>% 
-    pivot_longer(
-      cols = -c(SampleId, SampleCode, CountSum, data, score, n, minpc, est_n_minpc, est_min_minpc, est_max_minpc, n_taxa, score_best, est_n_direct), 
-      names_to = "taxon", 
-      values_to = "presence") %>%     
+  molten_download = target({
+      sqlite_file <- "data/define.sqlite"
+      download.file(
+        url = "https://github.com/richardjtelford/kustkarnor/blob/master/data/define.sqlite?raw=true", 
+        destfile = sqlite_file)
+      sqlite_file
+    },
+    format = "file"),
+  molten_data = {
+    con <- DBI::dbConnect(RSQLite::SQLite(), dbname = molten_download)
     
-    ggplot(aes(x = taxon, y = n, fill = presence)) + 
-    geom_raster(show.legend = FALSE) +
-    scale_y_continuous(limits = c(0, 300), expand = c(0, 0)) +
-    labs(y = "Putative count sum") +
-    coord_flip() +
-    scale_fill_manual(values = c("white", "black")),
+    tbl(con, "counts") %>%
+      #merge synonyms
+      left_join(tbl(con, "Synonyms"), by = c("taxonCode" = "synonymCode")) %>%
+      mutate(taxonCode = coalesce(correctCode, taxonCode)) %>%
+      #get country code for merges & drop samples with no chem - "RIB16102b"
+      inner_join(tbl(con, "fchem") %>% select(siteId, countryId), by  = "siteId") %>%
+      left_join(tbl(con, "Merges"), by = c("taxonCode" = "oldtaxonCode", "countryId")) %>%
+      mutate(
+        taxonCode = coalesce(mergedTaxonCode, taxonCode)
+      ) %>%
+      #remove excluded taxa
+      anti_join(tbl(con, "ExcludedTaxa"), by = "taxonCode") %>%
+      #select required columns
+      select(siteId, taxonCode, count) %>%
+      #sum merged taxa
+      group_by(siteId, taxonCode) %>%
+      collect() %>%
+      assert(not_na, everything()) %>%
+      summarise(count = sum(count, na.rm = TRUE)) # , na.rm = TRUE to avoid warning
+    
+    },
+  molten_singletons = molten_data %>% summarise_counts(), 
   
   ####diatom1####
   diatom1_data = if(has_password){
