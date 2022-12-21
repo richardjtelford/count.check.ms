c9159dc6e79953ca361b037f8f2ef8235cc0b482
richardjtelford <richard.telford@uib.no>
2022-12-21 16:10:56 +0100

    required packages moved to description

--- R/renv_extra_packages.R
+++
@@ -1,5 +0,0 @@
-#extra packages required but recognised by renv (probably suggests rather than imports field of package description file)
-requireNamespace("visNetwork")
-requireNamespace("rticles")
-requireNamespace("curl")
-requireNamespace("RSQLite")
