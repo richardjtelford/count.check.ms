12143d3f4034e5244bf879b27af97a26860aa64a
richardjtelford <richard.telford@uib.no>
2022-12-21 16:19:56 +0100

    clean code

--- R/bird_functions.R
+++ R/bird_functions.R
@@ -1,34 +1,37 @@
-#resample to smaller count sum with multivariate hypergeometric & count singletons
+# resample to smaller count sum with multivariate hypergeometric & count singletons
 recount_singletons <- function(counts, k, nrep = 10) {
-  k %>% set_names() %>%
-    map_df(~{
-      y <- extraDistr::rmvhyper(nn = nrep, n = counts, k = .)
+  k |>
+    set_names() |>
+    map(~ {
+      y <- extraDistr::rmvhyper(nn = nrep, n = counts, k = .x)
       tibble(no_singletons = rowSums(y == 1), rep = 1:nrep)
-    }, .id = "new_count_sum")
+    }) |>
+    list_rbind(names_to = "new_count_sum")
 }
 
-#functions
-#join and process at different taxonomic levels
+# functions
+# join and process at different taxonomic levels
 
 bird_process <- function(bird_data, bird_species) {
-  joined <- bird_data %>%
+  joined <- bird_data |>
     left_join(bird_species, by = "AOU")
-  
-  agg_taxa <- . %>% 
-    group_by(RouteDataID, taxon) %>%
-    summarise(count = sum(SpeciesTotal), .groups = "drop_last") 
-  
+
   out <- bind_rows(
-    species = joined %>% select(RouteDataID, taxon = Species, count = SpeciesTotal),  # no need to aggregate
-    genus = joined %>% rename(taxon = Genus) %>% agg_taxa,
-    family = joined %>% rename(taxon = Family) %>% agg_taxa,
-    order = joined %>% rename(taxon = Order) %>% agg_taxa,
+    species = joined |> select(RouteDataID, taxon = Species, count = SpeciesTotal), # no need to aggregate
+    genus = joined |> rename(taxon = Genus) |> agg_taxa(),
+    family = joined |> rename(taxon = Family) |> agg_taxa(),
+    order = joined |> rename(taxon = Order) |> agg_taxa(),
     .id = "taxonomic_level"
-  ) %>%
-    group_by(RouteDataID, taxonomic_level) %>%
+  ) |>
+    group_by(RouteDataID, taxonomic_level) |>
     summarise_counts()
 
   return(out)
 }
 
 
+agg_taxa <- function(df) {
+  df |>
+    group_by(RouteDataID, taxon) |>
+    summarise(count = sum(SpeciesTotal), .groups = "drop_last")
+}
