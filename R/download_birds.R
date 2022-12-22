cdd5f8b94c9caa17cc87b7b11e12f667a4ac7f6e
richardjtelford <richard.telford@uib.no>
2022-12-22 16:31:58 +0100

    birds workind

--- R/download_birds.R
+++ R/download_birds.R
@@ -1,26 +1,32 @@
-#breeding bird survey
-source("R/bird_functions.R")
+# breeding bird survey
 
-bird_plan <- drake_plan(
-  #download data
-  bird_download = {
-    bbs <- get_bbs_data(bbs_version = 2020, sb_dir = "data/birds2020")
+# download data
+download_bird <- function() {
+  bbs <- bbsAssistant::grab_bbs_data(
+    sb_id = "625f151ed34e85fa62b7f926",
+    bbs_dir = "data/birds2022"
+  )
 
-    #select required columns
-    bbs$observations %>% 
-      select(RouteDataID, AOU, SpeciesTotal)
-    },
+  # select required columns
+  bbs$observations |>
+    (\(x){
+      mutate(x, SpeciesTotal = rowSums(select(x, matches("^Stop\\d{1,2}"))))
+    })() |> 
+    select(RouteDataID, AOU, Year, SpeciesTotal)
+}
   
-  bird_species = read_table("data/birds2020/SpeciesList.txt", skip = 9) %>% 
-    slice(-1) %>% # row of dashes
-    select(-Seq, -French_Common_Name, -English_Common_Name, -Species) %>% 
-    rename(Species = Spanish_Common_Name, Order = ORDER), 
-  
-  #aggregate to different taxonomic levels
-  bird_singletons = bird_process(bird_download, bird_species),
 
-  bird_singleton_summary = bird_singletons %>%
-    group_by(taxonomic_level) %>%
+# load taxonomic data
+load_bird_species <- function(){ 
+  bbsAssistant::import_species_list("data/birds2022") |> 
+    select(AOU, Order = ORDER, Family, Genus, Species) |> 
+    mutate(Species = paste(Genus, Species))
+}
+
+
+summarise_bird_singletons <- function(bird_singletons) {
+  bird_singletons |>
+    group_by(taxonomic_level) |>
     summarise(
       p_singletons = mean(n_singletons > 0) * 100,
       n_singletons = sum(n_singletons == 0),
@@ -28,44 +34,53 @@ bird_plan <- drake_plan(
       n_routes = n(),
       GCD1 = mean(gcd == 1, na.rm = TRUE) * 100,
       GCD2plus = mean(gcd > 1, na.rm = TRUE) * 100,
-      GCD_max = max(gcd, na.rm = TRUE), 
+      GCD_max = max(gcd, na.rm = TRUE),
       .groups = "drop_last"
-    ) %>%
+    ) |>
     mutate(taxonomic_level = factor(
       taxonomic_level,
       levels = c("species", "genus", "family", "order")
-    )) %>%
-    arrange(taxonomic_level),
-  
-  bird_summ = list(
-    n_without_singletons = bird_singletons %>% filter(taxonomic_level == "species", n_singletons == 0) %>% nrow(),
-    n_GCD_gt1 = bird_singletons %>% filter(taxonomic_level == "species", gcd > 1 | is.na(gcd))
-    
-  ),
-  
-  #simulate counts with mv hypergeometric
-  bird_recount_singletons = bird_download %>% 
-    group_by(RouteDataID) %>% 
-    filter(sum(SpeciesTotal) >= 400) %>% 
-    left_join(bird_species, by = "AOU") %>% 
-    group_by(RouteDataID, Order) %>% 
-    summarise(count = sum(SpeciesTotal), .groups = "drop_last") %>% 
-    nest() %>% 
-    mutate(recount = map(data, ~recount_singletons(counts = .$count, k = c(30, 50, 100, 200, 300, 400)))) %>% 
-    unnest(cols = recount) %>% 
-    mutate(new_count_sum = as.numeric(new_count_sum)) %>% 
-    group_by(new_count_sum, rep) %>% 
-    summarise(m = mean(no_singletons == 0) * 100, .groups = "drop_last") %>% 
-    summarise(m_no_singletons = mean(m), sd = sd(m), se = sd/sqrt(n()), .groups = "drop_last"),
-  
-  bird_recount_singleton_plot = ggplot(
-      data = bird_recount_singletons, 
-      aes(x = new_count_sum, y = m_no_singletons, 
-          ymin = m_no_singletons - 1.96 * se, 
-          ymax = m_no_singletons + 1.96 * se)
-    ) + 
-    geom_errorbar() + 
+    )) |>
+    arrange(taxonomic_level)
+}
+
+summarise_bird <- function(bird_singletons) {
+  list(
+    n_without_singletons = bird_singletons |>
+      filter(taxonomic_level == "species", n_singletons == 0) |>
+      nrow(),
+    n_GCD_gt1 = bird_singletons |>
+      filter(taxonomic_level == "species", gcd > 1 | is.na(gcd))
+  )
+}
+
+# simulate counts with mv hypergeometric
+recount_bird_singletons <- function(bird_download, bird_species) {
+  bird_download |>
+    group_by(RouteDataID) |>
+    filter(sum(SpeciesTotal) >= 400) |>
+    left_join(bird_species, by = "AOU") |>
+    group_by(RouteDataID, Order) |>
+    summarise(count = sum(SpeciesTotal), .groups = "drop_last") |>
+    nest() |>
+    mutate(recount = map(data, ~ recount_singletons(counts = .x$count, k = c(30, 50, 100, 200, 300, 400)))) |>
+    unnest(cols = recount) |>
+    mutate(new_count_sum = as.numeric(new_count_sum)) |>
+    group_by(new_count_sum, rep) |>
+    summarise(m = mean(no_singletons == 0) * 100, .groups = "drop_last") |>
+    summarise(m_no_singletons = mean(m), sd = sd(m), se = sd / sqrt(n()), .groups = "drop_last")
+}
+
+plot_bird_recount_singleton_plot <- function(bird_recount_singletons) {
+  ggplot(
+    data = bird_recount_singletons,
+    aes(
+      x = new_count_sum, y = m_no_singletons,
+      ymin = m_no_singletons - 1.96 * se,
+      ymax = m_no_singletons + 1.96 * se
+    )
+  ) +
+    geom_errorbar() +
     geom_point(size = 0.5) +
     labs(x = "Count sum", y = "Percent without singletons")
-  )#end of drake_plan
- 
+}
--- _targets.R
+++ _targets.R
@@ -4,15 +4,23 @@
 #   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline # nolint
 
 # Load packages required to define the pipeline:
-library(targets)
-# library(tarchetypes) # Load other packages as needed. # nolint
+library("targets")
+library("tarchetypes")
 
 # Set target options:
 tar_option_set(
-  packages = c("tibble"), # packages that your targets need to run
+  packages = c("tidyverse", "patchwork", "readxl", "countSum", "assertr"), 
   format = "rds" # default storage format
   # Set other options as needed.
 )
+
+
+# ,
+# "rjt.misc",
+# "neotoma",
+# "neotoma2tibble",
+# "janitor",
+# "glue",
 
 # tar_make_clustermq() configuration (okay to leave alone):
 options(clustermq.scheduler = "multicore")
@@ -22,17 +30,59 @@ options(clustermq.scheduler = "multicore")
 
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
-# source("other_functions.R") # Source other scripts as needed. # nolint
+# tar_source("R/download_birds.R")
+# tar_source("R/download_testates.R")
+# tar_source("R/download_chironomids.R")
+# tar_source("R/download_neotoma.R")
+# tar_source("R/download_diatoms.R")
+# tar_source("R/download_marine.R")
+# #tar_source("R/download_neotoma_diatoms.R")
+# tar_source("R/planktic_forams_plan.R")
+# tar_source("R/percent_rounding_plan.R")
+
+#source("R/functions/general_functions.R") # Source other scripts as needed. # nolint
 
 # Replace the target list below with your own:
 list(
   tar_target(
-    name = data,
-    command = tibble(x = rnorm(100), y = rnorm(100))
+    #aggregate to different taxonomic levels
+    name = bird_download,
+    command = download_bird()
+  ),
+  
+  tar_target(
+    name = bird_species,
+    command = load_bird_species()
 #   format = "feather" # efficient storage of large data frames # nolint
   ),
+
   tar_target(
-    name = model,
-    command = coefficients(lm(y ~ x, data = data))
-  )
+    #aggregate to different taxonomic levels
+    name = bird_singletons,
+    command = bird_process(bird_download, bird_species)
+  ),
+
+
+  tar_target(
+    name = summarised_bird_singletons,
+    command = summarise_bird_singletons(bird_singletons)
+),
+
+
+tar_target(
+  name = recounted_bird_singletons,
+  command = recount_bird_singletons(bird_download, bird_species)
+),
+
+
+tar_target(
+  #aggregate to different taxonomic levels
+  name = summarised_bird,
+  command = summarise_bird(bird_singletons)
+),
+tar_target(
+  #aggregate to different taxonomic levels
+  name = recounted_bird_singletons_plot,
+  command = plot_bird_recount_singleton_plot(recounted_bird_singletons)
+)
 )
